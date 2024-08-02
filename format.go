6bb82e3fdc2c3470e324933e5e436858507d545c
unifuu <>
2024-08-02 13:22:16 +0900

    init

---
+++ format.go
@@ -0,0 +1,17 @@
+package format
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// ObjID converts a hexadecimal string to a MongoDB ObjectID.
+func ObjID(hex string) primitive.ObjectID {
+	// Attempt to convert the hex string to an ObjectID
+	objID, err := primitive.ObjectIDFromHex(hex)
+
+	if err != nil {
+		return primitive.NilObjectID
+	}
+
+	return objID
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/unifuu/gutils
+
+go 1.20
+
+require go.mongodb.org/mongo-driver v1.16.0
